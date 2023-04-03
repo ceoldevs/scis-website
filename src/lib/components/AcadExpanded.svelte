b6fb66247f9ab0c068b35fb1586b9f9e09ee85e6
smc181002 <meherchaitanya18802@gmail.com>
2023-04-03 12:57:39 +0530

    finished navigation with megamenu for laptop view

---
+++ src/lib/components/AcadExpanded.svelte
@@ -0,0 +1,77 @@
+
+<script>
+    export let root = "/acad";
+    let megamenu = false;
+    let style = "hidden";
+</script>
+
+<div class="cursor-pointer ">
+    <button class="flex items-center peer font-roboto-flex" on:click={() => (style == "hidden" ? style = "flex" : style = "hidden")}>
+        Academics
+        <span class="material-symbols-rounded">
+            {#if style == "hidden"}
+                keyboard_arrow_down
+            {:else}
+                keyboard_arrow_up
+            {/if}
+        </span>
+    </button>
+    <div class={"z-[10000] fixed w-screen top-34 pt-7 left-0 " + style}>
+        <div class="z-[1000] grid grid-cols-7 justify-items-center items-center lpt-lg:gap-x-16 lpt:gap-x-8 sm:gap-x-4 w-full lpt-lg:px-12 lpt:px-8 tbl:px-6 px-2 py-6 bg-background-light border-[0.5px] border-outline-light/20">
+            <div class="flex flex-col gap-y-4 col-span-4">
+                <h3> Programmes </h3>
+                <div class="flex flex-col gap-y-1">
+                    <a href={root + "/programmes/undergrad"} class="text-primary-light hover:underline decoration-primary-light decoration-1 text-xl">Undergraduate Programs</a>
+                    <span class="text-neutralvariant-40">
+                        Join us after 12th in our Integrated Masters of Technology through JoSSA. 
+                        <a 
+                            href={ root + "/programmes/undergrad/imtech" }
+                            class="text-primary-light underline decoration-primary-light decoration-1"
+                        >
+                            Check out more about this course
+                        </a>
+                    </span>
+                </div>
+                <div class="flex flex-col gap-y-1">
+                    <a href={root + "/programmes/postgrad"} class="text-primary-light hover:underline decoration-primary-light decoration-1 text-xl">Postgradate Programs</a>
+                    <span class="text-neutralvariant-40">
+                        Finished your GATE? join us with 
+                        <a 
+                            href={ root + "/programmes/postgrad/mtech" }
+                            class="text-primary-light underline decoration-primary-light decoration-1"
+                        >
+                            Masters
+                        </a> 
+                        and 
+                        <a 
+                            href={ root + "/programmes/postgrad/phd" }
+                            class="text-primary-light underline decoration-primary-light decoration-1"
+                        >
+                            Doctoral
+                        </a> 
+                        programs in Artificial Intelligence, Computer Science and Information Technologies in Banking.
+                    </span>
+                </div>
+            </div>
+            <div class="flex flex-col col-span-3 gap-y-6 justify-between">
+                <div class="flex flex-col">
+                    <a href= { root + "/admissions" } class="text-primary-light text-lg">
+                        Admissions
+                    </a>
+                    <span class="text-neutralvariant-40">
+                        Want to join UoH as a Computer Scientist? Learn more about our admission process
+                    </span>
+                </div>
+                <div class="flex flex-col">
+                    <a href= { root + "/programmes" } class="text-primary-light text-lg">
+                        Syllabus
+                    </a>
+                    <span class="text-neutralvariant-40">
+                        Checkout the syllabus of the course you are interested or studying in.
+                    </span>
+                </div>
+            </div>
+        </div>
+        <button class="z-[999] fixed h-screen w-screen bg-neutralvariant-10/40" on:click={() => (style == "hidden" ? style = "flex" : style = "hidden")}> </button>
+    </div>
+</div>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -4,6 +4,7 @@
 	// import {logicalPropertiesHorizontalSlide, horizontalSlide} from '$lib/components/slidetransitions'
 	// import {sublinks} from '$lib/stores/sublinks'
 	import {base} from "$app/paths";
+	import AcadExpanded from '$lib/components/AcadExpanded.svelte';
 
 	let active = $page.url.pathname;
 	let showMenu = false;
@@ -85,8 +86,6 @@
 				{name: "Admissions", link: "/admissions"},
 			],
 			// TODO: Write a resonable structure for this nested menu.
-			expandedinfo: {
-			}
 
 		},
 		{
@@ -99,59 +98,7 @@
 		},
 	]
 
-	// let sublinks = options[activeIndex].sublinks
-
 </script>
-
-<!-- {#if displayMenu}
-<div transition:logicalPropertiesHorizontalSlide={{direction: 'inline', duration: 500}} 
-	class="z-[10] fixed pt-28 w-screen h-screen flex bg-matty-900/90 backdrop-blur-lg px-24 overflow-hidden"
-	>
-	<div class="flex-1 flex flex-col gap-y-8">
-		{#each options as option, i }
-			<button 
-				class={ `w-min text-5xl flex font-semibold ${activePage(option.link, active)}` }
-				on:click={() => {
-					active = option.link
-					sublinks.set(option.sublinks)
-				}}
-			>
-				{#if option.sublinks?.length != 0}
-						<span>{option.name}</span>
-				{:else}
-						<a href={option.link}>{option.name}</a>
-				{/if}
-			</button>
-		{/each}
-	</div>
-	<div class="flex-1 flex flex-col gap-y-12">
-		{#if $sublinks.length != 0}
-			{#each $sublinks as sublink }
-				<a on:click={() => (displayMenu = false)} class={`${(sublink.link === $page.url.pathname) && "font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-sky-400"} w-fit text-xl`} href={sublink.link}>{sublink.name}</a>
-			{/each}
-		{/if}
-	</div>
-</div>
-{/if} -->
-<!-- <nav class="fixed border-b-2 border-matty-300/10 grid grid-cols-3 py-4 lpt-lg:px-24 lpt:px-14 px-4 w-screen z-[100] items-center bg-matty-50/70 backdrop-blur-md justify-items-end">
-	<span class="mbl-lg:flex justify-start tbl:hidden">
-		<button on:click={() => (displayMenu=!displayMenu)} class="material-symbols-rounded">
-			{#if displayMenu}
-				menu_open
-			{:else}
-				menu
-			{/if}
-		</button>
-	</span>
-	<div class="flex">
-		<a href={`${base}/`} class="flex items-center gap-3">
-			<img src={uohyd} alt="uoh-logo" >
-			<span class="font-medium max-w-1/2">School of Computer and Information Sciences</span>
-		</a>
-	</div>
-	<a href="#contact" class="flex w-full justify-end hover:underline underline-offset-2 decoration-2 decoration-sky-700">contact</a>
-
-</nav> -->
 
 <div class="fixed w-full z-[1000]">
 	<div class="font-roboto-flex flex w-full justify-between lpt:px-12 tbl:px-6 px-2 py-1 bg-primary-95 underline decoration-1 border-b-[0.5px] border-b-outline-light/20 text-sm">
@@ -177,6 +124,15 @@
 		</div>
 		<div>
 			<div class="gap-4 hidden sm:flex items-center">
+				{#each menu as {link, name, type} }
+					{#if type === "expandable"}
+						<AcadExpanded root={link} />
+					{:else}
+						<a href={link}>{name}</a>
+					{/if}
+				{/each}
+			</div>
+			<!-- <div class="gap-4 hidden sm:flex items-center">
 				<a href="/" class="text-primary-light">Home</a>
 				<a href="/news-events">News</a>
 				<div class="cursor-pointer group">
@@ -187,59 +143,12 @@
 					</span>
 					</div>
 					<div class="z-[10000] fixed w-screen top-34 pt-7 left-0 group-hover:block hidden">
-						<div class="grid grid-cols-7 justify-items-center gap-x-6 w-full lpt-lg:px-12 lpt:px-8 tbl:px-6 px-2 py-6 bg-background-light border-[0.5px] border-outline-light/20">
-							<div class="flex flex-col gap-y-4 col-span-4">
-								<h3> Programmes </h3>
-								<div class="flex flex-col gap-y-1">
-									<a href="/programmes/undergrad" class="text-primary-light text-xl">Undergraduate Programs</a>
-									<span class="text-neutralvariant-40">
-										Join us after 12th in our Integrated Masters of Technology through JoSSA. 
-										<a 
-											href="programmes/undergrad/imtech" 
-											class="text-primary-light underline decoration-primary-light decoration-1"
-										>
-											Check out more about this course
-										</a>
-									</span>
-								</div>
-								<div class="flex flex-col gap-y-1">
-									<a href="/programmes/postgrad" class="text-primary-light text-xl">Postgradate Programs</a>
-									<span class="text-neutralvariant-40">
-										Finished your GATE? join us with 
-										<a 
-											href="/programmes/postgrad/mtech" 
-											class="text-primary-light underline decoration-primary-light decoration-1"
-										>
-											Masters
-										</a> 
-										and 
-										<a 
-											href="/programmes/postgrad/phd" 
-											class="text-primary-light underline decoration-primary-light decoration-1"
-										>
-											Doctoral
-										</a> 
-										programs in Artificial Intelligence, Computer Science and Information Technologies in Banking.
-									</span>
-								</div>
-							</div>
-							<div class="flex flex-col col-span-3">
-								<div class="flex flex-col">
-									<a href="/admissions" class="text-primary-light text-lg">
-										Admissions
-									</a>
-									<span class="text-neutralvariant-40">
-										Want to join UoH as a Computer Scientist? Learn more about our admission process
-									</span>
-								</div>
-							</div>
-						</div>
 					</div>
 				</div>
 				<a href="/research">Research</a>
 				<a href="/careers-placements">Placements</a>
 				<a href="/about">About</a>
-			</div>
+			</div> -->
 			<button class="material-symbols-rounded flex sm:hidden" on:click={() => (showMenu = !showMenu) }>
 				{#if showMenu}
 					close
