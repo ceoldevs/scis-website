8d5e3806b0786c235d89bd78bfdd91cccd076034
Ankith Mall <122958600+AnkithMall@users.noreply.github.com>
2023-06-20 22:00:10 +0530

    skip to content added (#29)
    * skip to content added
    
    * Delete package-lock.json
    
    * Delete SkipToContent.svelte
    
    ---------
    
    Co-authored-by: Meher Chaitanya <53662575+smc181002@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,15 +3,19 @@
 	import Footer from './Footer.svelte';
 	import './styles.css';
 	// import AnimateSlide from "$lib/components/AnimateSlide.svelte";
+	
 </script>
 
-<div>
+<a class="fixed z-[2000] translate-y-[-100%] focus:translate-y-0 w-28 h-16 text-center text-white py-6 bg-primary-light text-xs" href='#main'>Skip To Content</a>	
+<div >
 	<Header />
 </div>
-<main class="pt-16 min-h-[90vh]">
+<main class="pt-16 min-h-[90vh]" id="main">
 	<slot />
 </main>
-<div class="mt-auto">
+<div class="mt-auto" id="footer">
 	<Footer />
 </div>
+
+
 	
