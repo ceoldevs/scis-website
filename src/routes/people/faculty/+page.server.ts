074b78411909946686951a2b21cbd6d5576797f2
ankith <20mcme19@uohyd.ac.in>
2023-06-27 15:40:49 +0530

    query string is converted to use qs instead of hard coded string

--- src/routes/people/faculty/+page.server.ts
+++ src/routes/people/faculty/+page.server.ts
@@ -1,8 +1,12 @@
+import qs from 'qs';
 export const prerender = true;
 
 export const load = async ({ fetch}: { fetch: any }) => {
+	const query = qs.stringify({
+		populate:'*'
+	})
 		try {
-			const res = await fetch(`http://127.0.0.1:1337/api/faculties?populate=*`);
+			const res = await fetch(`http://127.0.0.1:1337/api/faculties?${query}`);
 			const json = await res.json();
 
 			if (res.ok) {
--- src/routes/people/faculty/[slug]/+page.server.ts
+++ src/routes/people/faculty/[slug]/+page.server.ts
@@ -1,9 +1,18 @@
 import { error } from "@sveltejs/kit";
+import qs from 'qs';
 export const prerender = false;
 
 export const load = async ({ fetch, params }: { fetch: any; params: any }) => {
+  const query = qs.stringify({
+    filters : {
+      siteId:{
+        $eq:params.slug
+      }
+    },
+    populate:"*"
+  })
   // const url = `http://127.0.0.1:1337/api/news-events?filters\[Slug\][$eq]=s-tejaswi-awarded-for-the-best-paper-award-in-international-conference-on-distributed-computing-and-intelligent-technology-2023`
-  const url = `http://127.0.0.1:1337/api/faculties?filters[siteId][$eq]=${params.slug}&populate=*`;
+  const url = `http://127.0.0.1:1337/api/faculties?${query}`;
   try {
     const res = await fetch(url);
     const item = await res.json();
