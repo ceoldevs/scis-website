6bb40544a929e4a14e4e832d0ae763884c8d7d63
Ankith Mall <122958600+AnkithMall@users.noreply.github.com>
2023-06-28 10:41:27 +0530

    Query (#30)
    * skip to content added
    
    * query string is converted to use qs instead of hard coded string
    
    * deleted unnecesary files

--- src/routes/news-events/[slug]/+page.server.ts
+++ src/routes/news-events/[slug]/+page.server.ts
@@ -1,8 +1,17 @@
 export const prerender = false;
+import qs from 'qs';
 
 export const load = async ({ fetch, params }: {fetch: any, params: any}) => {
+  const query = qs.stringify({
+    filters : {
+      Slug:{
+        $eq:params.slug
+      }
+    },
+    populate:"*"
+  })
     // const url = `http://127.0.0.1:1337/api/news-events?filters\[Slug\][$eq]=s-tejaswi-awarded-for-the-best-paper-award-in-international-conference-on-distributed-computing-and-intelligent-technology-2023`
-    const url = `http://127.0.0.1:1337/api/news-events/?filters[Slug][$eq]=${params.slug}&populate=*`
+    const url = `http://127.0.0.1:1337/api/news-events/?${query}`
     try {
         const res = await fetch(url);
         const item = await res.json();
