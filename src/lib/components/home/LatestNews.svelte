0f856fac711d44985ce9f1d2839b29163aa7d7ad
ankith <20mcme19@uohyd.ac.in>
2023-09-24 17:32:12 +0530

    isse 39 complete

--- src/lib/components/home/LatestNews.svelte
+++ src/lib/components/home/LatestNews.svelte
@@ -6,6 +6,7 @@
 </script>
 <div class="pt-10 font-roboto-flex bg-on-primarycont-light relative flex flex-col lpt:flex-row">
     <div class={`tbl:relative w-full lpt:w-[50vw] lpt-lg:w-[58vw] lpt:py-5 pb-5 bg-on-primarycont-light lpt:bottom-0 lpt:h-[90vh] flex flex-col text-on-primary-light ${styleFixed}`}>
+		<a href={`/news-events/${first.attributes.Slug}`} >
 		<img 
 			class="object-cover w-full lpt:h-[50vh] h-64"
 			src={`http://localhost:1337${first.attributes.CoverImage.data.attributes.url}`}
@@ -14,7 +15,7 @@
 		<div class="lpt-lg:px-10 lpt:px-6 tbl:px-6 px-2 pt-4 flex flex-col gap-y-4">
 			<h1 class="font-fraunces lpt-lg:text-4xl lpt:text-3xl text-3xl">{first.attributes.Title}</h1>
 			<div>
-				{first.attributes.Content.slice(0,255)}...
+				{first.attributes.Description.slice(0,255)}...
 			</div>
 			<div class="">
 				{#each first.attributes.Actions as action}
@@ -22,10 +23,13 @@
 				{/each}
 			</div>
 		</div>
+	</a>
     </div>
     <div class="w-full ml-0 lpt:w-[50vw] lpt:ml-[50vw] lpt-lg:w-[42vw] lpt-lg:ml-[58vw] bg-primarycont-light flex flex-col">
 		<div class="py-8 lpt-lg:px-10 tbl:px-6 px-2 flex flex-col gap-y-6">
 			{#each other as item}
+			<a href={`/news-events/${item.attributes.Slug}`} >
+
 				<img 
 					class="object-cover w-full lpt-lg:h-80 lpt:h-56"
 					src={`http://localhost:1337${item.attributes.CoverImage.data.attributes.url}`}
@@ -34,7 +38,7 @@
 				<div class="flex flex-col gap-y-4">
 					<h2 class="font-fraunces text-2xl">{item.attributes.Title}</h2>
 					<div>
-						{item.attributes.Content.slice(0,155)}...
+						{item.attributes.Description.slice(0,155)}...
 					</div>
 					<div>
 						{#each item.attributes.Actions as action}
@@ -42,6 +46,7 @@
 						{/each}
 					</div>
 				</div>
+				</a>
 			{/each}
 		</div>
 		<div class="p-4 z-[100] bg-primary-20 flex items-center justify-between">
